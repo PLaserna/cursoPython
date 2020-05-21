8e4a9d0670a59998a61836e6824a09877db14122
PLaserna <59970656+PLaserna@users.noreply.github.com>
2020-05-21 12:01:12 +0200

    Delete admin.py

--- ejercicio10portal-peliculas/portal_peliculas/peliculas/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Pelicula, Genero, Usuario
-
-# Register your models here.
-
-admin.site.register(Pelicula)
-admin.site.register(Genero)
-admin.site.register(Usuario)
